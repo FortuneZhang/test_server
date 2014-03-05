65e7aa16158589fa35fe1d0a9483257210f3c39f
FortuneZhang <zhangfortune527@gmail.com>
2014-03-05 11:34:37 +0800

    fortune- init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ServerConnectionTest::Application.config.secret_key_base = 'd95fd3cf82cdabfce8fde399716027816854296376312dc8888cdfcc4a4fe858e0557cb3934f72896dd9bab3df9fb4eb80d16fde945154feb185efd6126efc13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ServerConnectionTest::Application.config.session_store :cookie_store, key: '_ServerConnectionTest_session'
